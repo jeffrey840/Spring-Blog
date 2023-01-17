6a840778242f4d07ef0fcdef9bb70262e63d2cb9
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-01-17 14:00:18 -0600

    completed controllers exercise

---
+++ src/main/java/com/codeup/springinitializer/MathController.java
@@ -0,0 +1,36 @@
+package com.codeup.springinitializer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    @RequestMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String add(@PathVariable int num1, @PathVariable int num2){
+        return num1 + " plus " + num2 + " = " + (num1 + num2);
+    }
+
+    @RequestMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public String subtract(@PathVariable int num1, @PathVariable int num2){
+        return num1 + " minus " + num2 + " = " + (num2 - num1);
+    }
+
+    @RequestMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiply(@PathVariable int num1, @PathVariable int num2){
+        return num1 + " multiplied by " + num2 + " = " + (num1 * num2);
+    }
+
+    @RequestMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public String divide(@PathVariable int num1, @PathVariable int num2){
+        return num1 + " divided by " + num2 + " = " + (num1 / num2);
+    }
+
+}
