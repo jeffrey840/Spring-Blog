af1cc4449fffc7c9c31f6ddfa2b1e754bdbc904b
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-01-18 14:57:20 -0600

    completed views exercise

--- src/main/java/com/codeup/springinitializer/PostController.java
+++ src/main/java/com/codeup/springinitializer/PostController.java
@@ -2,20 +2,31 @@ package com.codeup.springinitializer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import org.springframework.ui.Model;
 
 @Controller
 public class PostController {
 
-    @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String allPosts(){
-        return "This will be a page for all of the posts.";
+    @GetMapping("/posts")
+    public String allPosts(Model model){
+        ArrayList<Post> all = new ArrayList<>(2);
+        Post post1 = new Post("post #1 title", "post #1 body");
+        Post post2 = new Post("post #2 title", "post #2 body");
+        all.add(post1);
+        all.add(post2);
+        model.addAttribute("title", "All Posts");
+        model.addAttribute("allPosts", all);
+        return "posts/index";
     }
 
-    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String post(@PathVariable int id){
-        return "This will return an individual post with the id of " + id;
+    @GetMapping(path = "/posts/{id}")
+    public String post(Model model){
+        Post post1 = new Post("post #1 title", "post #1 body");
+        model.addAttribute("title", "Individual Post");
+        model.addAttribute("postTitle", post1.getTitle());
+        model.addAttribute("postBody", post1.getBody());
+        return "posts/show";
     }
 
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
