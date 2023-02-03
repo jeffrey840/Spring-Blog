d2f765ea9fb6eb8b761e1b75ee0cb65ca7069e1a
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-02-02 22:12:45 -0600

    testing endpoints

--- ignore/script.js
+++ ignore/script.js
@@ -1,1 +1,16 @@
-alert("connected")
+
+// endpoints
+
+//testing an endpoint
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '',
+		'X-RapidAPI-Host': 'zillow-com1.p.rapidapi.com'
+	}
+};
+
+fetch('https://zillow-com1.p.rapidapi.com/propertyExtendedSearch?location=santa%20monica%2C%20ca&home_type=Houses', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
